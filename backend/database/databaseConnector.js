f88fd637fa3184ace2175e97e2d0508b0ed6eabe
Vedang Gupta <124082944+VedangGupta1@users.noreply.github.com>
2024-10-27 00:02:57 +0530

    Update databaseConnector.js

--- backend/database/databaseConnector.js
+++ backend/database/databaseConnector.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const ConnectDatabase = (connection_url) => {
-  mongoose.connect("mongodb+srv://vedang12:vedang12@cluster0.nx5oect.mongodb.net/Library?retryWrites=true&w=majority&appName=Cluster0")
+  mongoose.connect("")
 }
 
 module.exports = { ConnectDatabase }
